f2b6e0ab27a95800eb1c08e220e42d7b61c35403
Miłosz Andrzej Piekarski <milosz.piekarski2003@gmail.com>
2022-09-15 20:19:18 +0200

    Working Bottom Navigation Bar with fragment swap

---
+++ VirtualPantry/app/src/main/java/com/example/virtualpantry/FragmentEdit.kt
@@ -0,0 +1,7 @@
+package com.example.virtualpantry
+
+import androidx.fragment.app.Fragment
+
+class FragmentEdit : Fragment(R.layout.fragment_edit) {
+
+}
--- VirtualPantry/app/src/main/java/com/example/virtualpantry/MainActivity.kt
+++ VirtualPantry/app/src/main/java/com/example/virtualpantry/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.virtualpantry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Fragmenty
+        val fragmentPantry = FragmentPantry()
+        val fragmentAdd = FragmentAdd()
+        val fragmentEdit = FragmentEdit()
+
+//        Ustawienie pierwszego ekranu
+        replaceCurrentFragment(fragmentPantry)
+
+//        Pasek nawigacyjny
+        val navigationBarView = findViewById<BottomNavigationView>(R.id.bnvMenuBar)
+
+        navigationBarView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.pantryMenu -> {
+                    replaceCurrentFragment(fragmentPantry)
+                    true
+                }
+                R.id.addMenu -> {
+                    replaceCurrentFragment(fragmentAdd)
+                    true
+                }
+                R.id.editMenu -> {
+                    replaceCurrentFragment(fragmentEdit)
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+
     }
+
+    private fun replaceCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flContainer, fragment)
+            commit()
+        }
 }
