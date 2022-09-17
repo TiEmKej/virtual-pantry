a11c04fa93844d41a86aff0512394f8f94ee44ec
Miłosz Andrzej Piekarski <milosz.piekarski2003@gmail.com>
2022-09-17 11:00:23 +0200

    Remove Old Version

--- virtual-pantry-app/app/src/main/java/com/example/virtualpantry/PantryItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.virtualpantry
-
-import android.media.Image
-import java.sql.Date
-
-data class PantryItem(val itemName: String, val itemQuantity: Int, val itemPhoto: Image, val itemRegDate: Date)
