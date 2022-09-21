f9148bb8deb57d12c6d2c4119b649858d63868cb
Miłosz Andrzej Piekarski <milosz.piekarski2003@gmail.com>
2022-09-21 11:30:22 +0200

    RecycleView działa

--- VirtualPantry/app/src/main/java/com/example/virtualpantry/fragments/FragmentEdit.kt
+++ VirtualPantry/app/src/main/java/com/example/virtualpantry/fragments/FragmentEdit.kt
@@ -1,6 +1,7 @@
-package com.example.virtualpantry
+package com.example.virtualpantry.fragments
 
 import androidx.fragment.app.Fragment
+import com.example.virtualpantry.R
 
 class FragmentEdit : Fragment(R.layout.fragment_edit) {
 
